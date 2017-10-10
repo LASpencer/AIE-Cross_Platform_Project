7891980b8f63a0134544057296950e06a2748d4a
Leonard Andrew Spencer <thrawn369@gmail.com>
2017-10-10 10:31:42 +1030

    Added main menu screen

---
+++ Project/Cross Platform Project/Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController {
+
+    private static GameController instance;
+
+    public static GameController Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = new GameController();
+            }
+            return instance;
+        }
+    }
+
+    public int LastScore = 0;
+
+    public int HighScore = 0;
+
+    private GameController()
+    {
+
+    }
+}
