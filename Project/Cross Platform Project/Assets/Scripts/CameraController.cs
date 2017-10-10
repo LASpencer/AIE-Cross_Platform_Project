39139489d083619dc9a67a3bb8a0f7381710f274
Leonard Andrew Spencer <thrawn369@gmail.com>
2017-10-10 12:03:42 +1030

    Added camera controller to set player's screen position

---
+++ Project/Cross Platform Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraController : MonoBehaviour {
+
+    public Vector3 TargetPosition;
+    public GameObject Player;
+
+    Camera cam;
+
+	// Use this for initialization
+	void Start () {
+        cam = gameObject.GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 playerPos = Player.transform.position;
+        Ray targetRay = cam.ScreenPointToRay(TargetPosition);
+        Plane playerPlane = new Plane(transform.forward, playerPos);
+        float distance;
+        playerPlane.Raycast(targetRay, out distance);
+        Vector3 point = targetRay.GetPoint(distance);
+        Vector3 offset = playerPos - point;
+        transform.position += offset;
+	}
+}
